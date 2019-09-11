926d8c6a67f3be174f994981d2a78a60fbbc0d67
Divya Beech <dyadav1006@gmail.com>
2019-09-11 00:53:44 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -4,12 +4,12 @@ next_colors = "Black"
 different_colors.push(next_colors)
 end
 
-def using_pop(different_colors)
+def pop(different_colors)
 different_colors = ["Red", "White", "Blue", "Orange"]
 different_colors.pop
 end
 
-def using_unshift(different_colors)
+def unshift(different_colors)
 different_colors = ["Red", "White", "Blue", "Orange"]
 different_colors.unshift("Brown")
 p colors
@@ -27,7 +27,7 @@ end
 
 def shift_with_args(different_colors)
 different_colors = ["Red", "White", "Blue", "Orange"]
-change_of_mind = different_colors.shift
+change_of_mind = different_colors.shift_with_args
 end
 
 
@@ -35,4 +35,14 @@ def using_concat(my_favorite_things, more_fav)
   my_favorite_things = ["raindrops on roses", "whiskers on kittens"]
   more_pref = ["mario kart", "flatiron school"]
   all_my_favs = my_favorite_things.concat
+end
+
+def using_uniq(different_colors)
+  different_colors = ["Blue", "White", "Red", "Brown", "Blue"]
+  different_colors.uniq
+end
+
+def using_insert(different_numbers)
+  different_numbers = [1, 2, 3, 4,]
+  different_numbers = [4, 4,5]
 end
